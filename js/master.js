5b15f471e5ef932cf2fde08c4e8ea92d9fa54a06
trwinowiecki <trw0511@gmail.com>
2019-03-03 15:24:29 -0500

    modal customization and font changes

--- js/master.js
+++ js/master.js
@@ -77,30 +77,23 @@ masonryEvents.forEach( function(event) {
 /* Do a resize once more when all the images finish loading */
 waitForImages();
 
-$(".title").innerHTML = "test";
+// Apply open and close funstions to call modals
+$('.modal').each((index, element) => {
+  let target_id = $(element).attr('target');
+  let card = $(`.card#${target_id}`);
+  let close = $(element).find('.close');
 
-// Get the modal
-var modal = document.getElementById('myModal');
+  card.on('click', (event) => {
+    element.style.display = "block";
+  });
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+  close.on('click', (event) => {
+    element.style.display = "none";
+  });
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  window.onclick = (event) => {
+    if (event.target == element) {
+      element.style.display = "none";
+    }
   }
-}
+});
